a4e71bc337f7f9ae0904e5bd95fe949832de1790
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-11-11 13:12:58 -0300

    Add more validations

--- malli/src/validation.clj
+++ malli/src/validation.clj
@@ -19,5 +19,29 @@
      [:x :boolean]
      [:y {:optional true} :int]
      [:z :string]]))
-
 (valid? {:x true, :z "hello"})
+
+;; maps are open by default
+(m/validate
+ [:map [:x :int]]
+ {:x 1, :extra "key"})
+
+;; maps can be closed with :closed property
+(m/validate
+ [:map {:closed true} [:x :int]]
+ {:x 1, :extra "key"})
+
+;; maps keys are not limited to keywords
+(m/validate
+ [:map
+  ["status" [:enum "ok"]]
+  [1 :any]
+  [nil :any]
+  [::a :string]]
+ {"status" "ok"
+  1 'number
+  nil :yay
+  ::a "properly awesome"})
+
+;; most core-predicates are mapped to schemas
+(m/validate string? "hello")
