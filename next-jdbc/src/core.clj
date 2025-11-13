8ce714a166674d9e672d564350fec83f0fca552b
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-11-13 18:39:29 -0300

    Implement next-jdbc POC

--- next-jdbc/src/core.clj
+++ next-jdbc/src/core.clj
@@ -1,1 +1,21 @@
-(ns core)
+(ns core
+  (:require [next.jdbc :as jdbc]))
+
+(def db {:dbtype "h2"
+         :dbname "example"})
+
+(defonce ds (jdbc/get-datasource db))
+
+(comment
+  (jdbc/execute! ds ["
+create table address (
+  id int auto_increment primary key,
+  name varchar(32),
+  email varchar(255)
+)"])
+  (jdbc/execute! ds ["
+insert into address (name, email)
+  values ('Rafael', 'rafael@email.com')
+"])
+  (jdbc/execute! ds ["select * from address"])
+  )
