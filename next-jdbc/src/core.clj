fadd69db338bce7f55bec2f156f9d3e76bb78379
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-11-20 11:32:02 -0300

    Improve code format

--- next-jdbc/src/core.clj
+++ next-jdbc/src/core.clj
@@ -9,27 +9,27 @@
 
 (comment
   (jdbc/execute! ds ["
-create table address (
-  id int auto_increment primary key,
-  name varchar(32),
-  email varchar(255)
-)"])
+    create table address (
+      id int auto_increment primary key,
+      name varchar(32),
+      email varchar(255)
+    )"])
   (jdbc/execute! ds ["
-insert into address (name, email)
-  values ('Rafael', 'rafael@email.com')
-"])
+    insert into address (name, email)
+      values ('Rafael', 'rafael@email.com')
+    "])
   (jdbc/execute! ds ["select * from address"])
 
   (jdbc/execute-one! ds ["
-insert into address (name, email)
-  values ('Someone Else', 'some@elsewhere.com')
-"] {:return-keys true})
+    insert into address (name, email)
+      values ('Someone Else', 'some@elsewhere.com')
+    "] {:return-keys true})
   (jdbc/execute-one! ds ["select * from address where id = ?" 2])
 
   (jdbc/execute-one! ds ["
-insert into address (name, email)
-  values ('Someone Else', 'some@elsewhere.com')
-"] {:return-keys true :builder-fn rs/as-unqualified-lower-maps})
+    insert into address (name, email)
+      values ('Someone Else', 'some@elsewhere.com')
+    "] {:return-keys true :builder-fn rs/as-unqualified-lower-maps})
   (jdbc/execute-one! ds ["select * from address where id = ?" 3]
                      {:builder-fn rs/as-unqualified-lower-maps})
 
