c97969e0cfbe8fab08950f7cd64b5664a6423b6b
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-01 13:45:13 -0300

    Implement SICP's cons

---
+++ sicp/src/cons.clj
@@ -0,0 +1,18 @@
+(ns cons)
+
+(defn cons [a b]
+  (fn [pick]
+    (cond (= pick 1) a
+          (= pick 2) b)))
+
+(defn car [x]
+  (x 1))
+
+(defn cdr [x]
+  (x 2))
+
+(comment
+  (def a (cons 37 49))
+  (car a)
+  (cdr a)
+  )
