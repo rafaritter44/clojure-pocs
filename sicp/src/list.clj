95a39627345ab3733bfe9d35728527a0cdcd3aa6
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-02 10:50:56 -0300

    Implement SICP's list

---
+++ sicp/src/list.clj
@@ -0,0 +1,16 @@
+(ns list
+  (:require [cons :refer [cons]]
+            [for-each :refer [for-each]]))
+
+(defn list [& items]
+  (if (empty? items)
+    nil
+    (cons (first items)
+          (apply list (rest items)))))
+
+(def one-to-four
+  (list 1 2 3 4))
+
+(comment
+  (for-each println one-to-four)
+  )
