85bf0d8c63b3b71a1195f200aac9fceafd1e5c99
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-15 16:47:42 -0300

    Implement second version of cons

--- sicp/src/cons.clj
+++ sicp/src/cons.clj
@@ -4,12 +4,17 @@
   (fn [pick]
     (cond (= pick 1) a
           (= pick 2) b)))
-
 (defn car [x]
   (x 1))
-
 (defn cdr [x]
   (x 2))
+
+(defn cons [x y]
+  (fn [m] (m x y)))
+(defn car [x]
+  (x (fn [a d] a)))
+(defn cdr [x]
+  (x (fn [a d] d)))
 
 (comment
   (def a (cons 37 49))
