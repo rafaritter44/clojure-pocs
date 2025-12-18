3bdf387a5d3d593bbe578bf1ba07b9401b6098b2
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-18 12:23:32 -0300

    Implement atom POCs

--- atoms/src/core.clj
+++ atoms/src/core.clj
@@ -1,1 +1,31 @@
 (ns core)
+
+;; atom
+(def counter (atom 0))
+
+(comment
+  ;; deref
+  (deref counter)
+  @counter
+
+  ;; swap!
+  (swap! counter inc)
+  (swap-vals! counter inc)
+
+  ;; reset!
+  (reset! counter 0)
+  (reset-vals! counter 0)
+
+  ;; compare-and-set!
+  (compare-and-set! counter 0 1)
+
+  ;; set-validator!
+  (set-validator! counter (every-pred number? (complement neg?)))
+  (reset! counter -1)
+
+  ;; add-watch
+  (add-watch counter :watcher
+             (fn [key atom old-state new-state]
+               (println "Counter changed from" old-state "to" new-state)))
+  (remove-watch counter :watcher)
+  )
