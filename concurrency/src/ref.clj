e2dc325d95d8ec173f42c7d65462a8bd06763a81
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-19 13:16:13 -0300

    Add commute and ref-set to ref POC

--- concurrency/src/ref.clj
+++ concurrency/src/ref.clj
@@ -2,28 +2,39 @@
 
 (def account-validator (every-pred number? (complement neg?)))
 
-(defn ->accounts []
-  {:checking (ref 100M :validator account-validator)
-   :savings  (ref 0M :validator account-validator)})
+(defn ->account []
+  {:checking                 (ref 100M :validator account-validator)
+   :savings                  (ref 0M :validator account-validator)
+   :transfers-sent-today     (ref 0)
+   :transfers-received-today (ref 0)})
 
-(defn balances [a]
+(defn balance [a]
   (update-vals a deref))
 
 (defn transfer [from from-type to to-type amount]
   (dosync
    (alter (from-type from) - amount)
-   (alter (to-type to) + amount)
    (when (< (+ @(:checking from) @(:savings from)) 50M)
      (throw (Exception. "Combined balance too low")))
-   {:from (balances from)
-    :to   (balances to)}))
+   (alter (to-type to) + amount)
+   (commute (:transfers-sent-today from) inc)
+   (commute (:transfers-received-today to) inc)
+   {:from (balance from)
+    :to   (balance to)}))
 
-(def a1 (->accounts))
-(def a2 (->accounts))
+(defn reset-transfers-count [a]
+  (dosync
+   (ref-set (:transfers-sent-today a) 0)
+   (ref-set (:transfers-received-today a) 0)))
+
+(def a1 (->account))
+(def a2 (->account))
 
 (comment
-  (balances a1)
-  (balances a2)
+  (balance a1)
+  (balance a2)
   (transfer a1 :checking a2 :savings 10M)
   (transfer a2 :savings a1 :checking 20M)
+  (reset-transfers-count a1)
+  (reset-transfers-count a2)
   )
