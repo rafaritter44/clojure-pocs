74ef2ef457e891d4b769739c9d09e5af260d3b38
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-22 19:06:38 -0300

    Implement agent POC

---
+++ concurrency/src/agent.clj
@@ -0,0 +1,46 @@
+(ns agent)
+
+(defn ->agent []
+  (agent 0
+         :validator (every-pred number? (complement neg?))
+         :error-handler (fn [a e] (println {:agent @a :error (.getMessage e)}))
+         :error-mode :continue))
+
+(defn watch [key agent old-state new-state]
+  (println "Counter changed from" old-state "to" new-state))
+
+(def a1 (->agent))
+(def a2 (->agent))
+
+(comment
+  ;; deref
+  (deref a1)
+  @a2
+
+  ;; send
+  (send a1 inc)
+  (send-off a2 inc)
+
+  ;; await
+  (await a1 a2)
+  (await-for 10000 a1 a2)
+
+  ;; add-watch
+  (add-watch a1 :watch watch)
+  (add-watch a2 :watch watch)
+  (remove-watch a1 :watch)
+  (remove-watch a2 :watch)
+
+  ;; restart-agent
+  (restart-agent a1 0)
+  (restart-agent a2 0)
+
+  ;; shutdown-agents
+  (shutdown-agents)
+
+  ;; *agent*
+  *agent*
+
+  ;; release-pending-sends
+  (release-pending-sends)
+  )
