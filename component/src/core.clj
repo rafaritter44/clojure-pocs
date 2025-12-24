46adfd52e3d4dec8d2c2999c6f98e50c6bd99ff2
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-24 12:54:58 -0300

    Add database component functions

--- component/src/core.clj
+++ component/src/core.clj
@@ -17,3 +17,15 @@
 
 (defn new-database [host port]
   (map->Database {:host host :port port}))
+
+(def execute-query (constantly {:username "Rafael" :email "rafael@ritter.com"}))
+(def execute-insert (constantly true))
+
+(defn get-user [database username]
+  (execute-query (:connection database)
+                 "SELECT * FROM users WHERE username = ?"
+                 username))
+(defn add-user [database username email]
+  (execute-insert (:connection database)
+                  "INSERT INTO users (username, email)"
+                  username email))
