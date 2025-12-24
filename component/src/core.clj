f723d5f4cfd37a55338984379bfcf7a33c0acb83
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-12-24 12:47:45 -0300

    Implement database component

---
+++ component/src/core.clj
@@ -0,0 +1,16 @@
+(ns core
+  (:require [com.stuartsierra.component :as component]))
+
+(def connect-to-database (constantly "Connection"))
+
+(defrecord Database [host port connection]
+  component/Lifecycle
+  (start [component]
+    (println "Starting database...")
+    (let [conn (connect-to-database host port)]
+      (assoc component :connection conn)))
+  (stop [component]
+    (println "Stopping database...")
+    (.close connection)
+    ;; If you dissoc one of the base fields from a record, you get a plain map.
+    (assoc component :connection nil)))
