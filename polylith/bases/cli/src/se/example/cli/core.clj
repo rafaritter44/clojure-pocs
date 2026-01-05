22f8f8020f99b982e5230d354b513c14c978ea45
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-05 13:16:53 -0300

    Implement cli base in Polylith POC

--- polylith/bases/cli/src/se/example/cli/core.clj
+++ polylith/bases/cli/src/se/example/cli/core.clj
@@ -1,2 +1,7 @@
-(ns se.example.cli.core)
+(ns se.example.cli.core
+  (:require [se.example.user.interface :as user])
+  (:gen-class))
 
+(defn -main [& args]
+  (println (user/hello (first args)))
+  (System/exit 0))
