16cf6015156709263b19a7bf9d717c33c18df924
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-11 12:52:58 -0300

    Implement third version of The Little Schemer's recursive length without define

--- the-little-schemer/src/core.clj
+++ the-little-schemer/src/core.clj
@@ -29,3 +29,21 @@
           :else (inc (length (rest l)))))))
    '('apples))
   )
+
+(defn Y
+  "The applicative-order Y combinator"
+  [le]
+  ((fn [f] (f f))
+   (fn [f]
+     (le (fn [x] ((f f) x))))))
+
+(comment
+  ;; Third version
+  ((Y
+    (fn [length]
+      (fn [l]
+        (cond
+          (empty? l) 0
+          :else (inc (length (rest l)))))))
+   '('apples))
+  )
