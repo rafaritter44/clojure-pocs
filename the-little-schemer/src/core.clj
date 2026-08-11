11c7816523c190d1c420e5508ff358fd042d926d
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-11 12:47:00 -0300

    Implement second version of The Little Schemer's recursive length without define

--- the-little-schemer/src/core.clj
+++ the-little-schemer/src/core.clj
@@ -14,4 +14,18 @@
                  ((mk-length mk-length)
                   (rest l)))))))
    '('apples))
+
+  ;; Second version
+  (((fn [le]
+      ((fn [mk-length]
+         (mk-length mk-length))
+       (fn [mk-length]
+         (le (fn [x]
+               ((mk-length mk-length) x))))))
+    (fn [length]
+      (fn [l]
+        (cond
+          (empty? l) 0
+          :else (inc (length (rest l)))))))
+   '('apples))
   )
